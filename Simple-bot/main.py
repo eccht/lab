469eddce51e53895041bfa9821515809c4c27de7
eccht <ahtimirov@outlook.com>
2022-10-05 17:20:46 +0300

    v3

---
+++ Simple-bot/main.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import types
+
+token = "5731016264:AAHdiKIkYnsi7aEf8BF5WjcBMRaqrhxE8P0"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу", "/help")
+    bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею...')
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда тебе сюда - https://mtuci.ru/')
+
